fcd08011659bed13f0591038e4cf36437d4a9487
LevyFranco <levy@casintec.com.br>
2020-02-12 12:22:41 -0300

    add arquivo exemplo02.php padrão kemuel-kesley

---
+++ phpCompleto/variaveis/exemplo02.php
@@ -0,0 +1,14 @@
+<?php 
+  //padrão de escrita de variáveis kamuel case
+
+  $anoNascimento = 1988;
+
+  $nomeCompleto = "Wanderson Levy Franco dos Santos";
+
+  echo $anoNascimento;
+
+  echo "<br />";
+
+  echo $nomeCompleto;
+
+?>
