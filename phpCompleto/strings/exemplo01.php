ad71ba173014477dca4bf636c8df4192c95013c1
LevyFranco <levy@casintec.com.br>
2020-02-19 17:01:50 -0300

    add exemplo01 de Strings

---
+++ phpCompleto/strings/exemplo01.php
@@ -0,0 +1,18 @@
+<?php
+  // Video Aula 18
+  //STRINGS 
+  $nome = "Levy Franco";
+  $nome2 = "Levy Santos";
+  $nome3 = "Wanderson Levy";
+
+  //Diferença entre string com aspas simples e strings com aspas duplas
+
+
+
+  echo '<p>Casintec com '.$nome.'</p>';
+  echo '<p>Casintec com '.$nome2.'</p>';
+  echo "<p>Casintec com $nome3</p>";
+
+  //var_dump($nome, $nome2);
+
+?>
