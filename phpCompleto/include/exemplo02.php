e466920999c899d245ec63d26f67e9b2d130bd33
LevyFranco <levy@casintec.com.br>
2020-02-20 12:22:33 -0300

    ajustes de exemplo02

--- phpCompleto/include/exemplo02.php
+++ phpCompleto/include/exemplo02.php
@@ -6,4 +6,15 @@
 
   echo $resultado;
 
+  /*
+  Qual a diferença entre include e require?
+    R=  Com require é necessário que o arquivo exista e que o arquivo
+        estejá funcionando corretamente. Senão o require gera um erro 
+        fatal, parando a execução do código.
+
+        O include tenta prosseguir a execução do código mesmo se não
+        existir o arquivo ou se o arquivo estiver com erros.
+
+        
+  */
 ?>
