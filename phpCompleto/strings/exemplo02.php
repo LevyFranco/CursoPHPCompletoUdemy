d0289eae678d7cd3561e64b6a9dd67ddabae1d41
LevyFranco <levy@casintec.com.br>
2020-02-20 07:33:58 -0300

    add exemplo02

---
+++ phpCompleto/strings/exemplo02.php
@@ -0,0 +1,13 @@
+<?php
+  // Video Aula 18
+  //FUNÇÕES PARA STRINGS 
+
+  $nome = "Wanderson Levy Franco dos Santos";
+
+  //função strtoupper - para deixar todas as letras maiúsculas
+  echo strtoupper("<p>$nome</p>");
+
+  //função strtoupper - para deixar todas as letras maiúsculas
+  echo strtolower("<p>$nome</p>");
+
+?>
