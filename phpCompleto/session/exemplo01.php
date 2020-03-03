360b5b9cf1aab1ba1d39c69093c0e8be57741f27
LevyFranco <levy@casintec.com.br>
2020-03-03 19:38:51 -0300

    alteração de exemplo01 e add exemplo02

--- phpCompleto/session/exemplo01.php
+++ phpCompleto/session/exemplo01.php
@@ -1,7 +1,17 @@
 <?php
 
-session_start();
+  //Video Aula 28 - Criando e Entendendo Sessões em PHP
+  //session_start | session_unset | session_destroy
 
-$_SESSION[''];
+  //Serve para iniciar uma sessão
+  session_start();
+
+  //Função que serve para LIMPAR o usuário da sessão
+  session_unset('nome');
+
+  $_SESSION["nome"] = "Casintec";
+
+  //Variável que serve para LIMPAR a variável e remover o usuário da sessão
+  session_destroy();
 
 ?>
---
+++ phpCompleto/session/exemplo02.php
@@ -0,0 +1,11 @@
+<?php
+
+  //Video Aula 28 - Criando e Entendendo Sessões em PHP
+  //session_start
+
+  //Serve para iniciar uma sessão
+  session_start();
+
+  echo $_SESSION["nome"];
+
+?>
